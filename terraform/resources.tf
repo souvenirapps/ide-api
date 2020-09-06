b517a1e067d9e7f758bdbf37e8c61e4a2be90799
Faisal Alam <faisal.alam@rentomojo.com>
2020-09-07 03:07:37 +0530

    chore: cloud run version dynamic name

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -10,6 +10,10 @@ resource "google_cloud_run_service" "ide_api" {
   }
 
   template {
+    metadata {
+      name = "ide-api-app-${substr(sha1(timestamp()), 0, 7)}"
+    }
+
     spec {
       containers {
         image = "gcr.io/${var.project_id}/${var.app_docker_image_name}"
@@ -59,6 +63,7 @@ resource "google_cloud_run_service_iam_member" "member" {
 
 resource "google_cloud_scheduler_job" "warm_ide_api" {
   provider = google-beta
+  region   = "us-central1"
 
   name        = "warm-ide-api"
   description = "Ping IDE API Cloud Run app to prevent cold start."
